4103e5c187f478d46cf4bc191b68cf204f750f1f
vtu22732 <vtu22732@veltech.edu.in>
2025-09-08 16:19:51 +0530

    Create 6b.java

---
+++ task6/6b.java
@@ -0,0 +1,44 @@
+public class RadixSort {
+    public static int getMax(int[] arr) {
+    int max = arr[0];
+    for (int i = 1; i < arr.length; i++) {
+    if (arr[i] > max) {
+    max = arr[i];
+    }
+    }
+    return max;
+    }
+    public static void countingSort(int[] arr, int exp) {
+    int n = arr.length;
+    int[] output = new int[n];
+    int[] count = new int[10];
+    for (int i = 0; i < n; i++) {
+    count[(arr[i] / exp) % 10]++;
+    }
+    for (int i = 1; i < 10; i++) {
+    count[i] += count[i - 1];
+    }
+    for (int i = n - 1; i >= 0; i--) {
+    output[count[(arr[i] / exp) % 10] - 1] = arr[i];
+    count[(arr[i] / exp) % 10]--;
+    }
+    for (int i = 0; i < n; i++) {
+    arr[i] = output[i];
+    }
+    }
+    public static void radixSort(int[] arr) {
+    int max = getMax(arr);
+    for (int exp = 1; max / exp > 0; exp *= 10) {
+    countingSort(arr, exp);
+    }
+    }
+    public static void main(String[] args) {
+    int[] arr = {170, 45, 75, 90, 802, 24, 2, 66};
+    radixSort(arr);
+    System.out.println("Sorted array: ");
+    for (int num : arr) {
+    System.out.print(num + " ");
+    }
+    }
+    }
+    
